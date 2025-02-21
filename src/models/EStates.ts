bcdb8277c564ca20d143f45359f919cb2842bd31
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-02-21 14:52:58 +0100

    Add initial GameEndpoint and game state enums
    Introduce `GameEndpoint` to handle player and game actions, including methods for determining outcomes and generating random plays. Define enums in `EStates` for game states, statuses, and hands to support game logic.

---
+++ src/models/EStates.ts
@@ -0,0 +1,18 @@
+export enum EGameState {
+    Won,
+    Lost,
+    Draw
+}
+
+export enum EGameStatus {
+    Ready,
+    InProgress,
+    Finished,
+    Ended
+}
+
+export enum EGameHand {
+    Rock,
+    Paper,
+    Scissors
+}
